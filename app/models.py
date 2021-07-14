7a3cba737bc4c77fcd9c3621098e8435e2eef412
Naman-18 <namanrockstar18@gmail.com>
2021-07-14 17:53:38 +0530

    Initializing Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+STATE_CHOICES = (
+   ("AN","Andaman and Nicobar Islands"),
+   ("AP","Andhra Pradesh"),
+   ("AR","Arunachal Pradesh"),
+   ("AS","Assam"),
+   ("BR","Bihar"),
+   ("CG","Chhattisgarh"),
+   ("CH","Chandigarh"),
+   ("DN","Dadra and Nagar Haveli"),
+   ("DD","Daman and Diu"),
+   ("DL","Delhi"),
+   ("GA","Goa"),
+   ("GJ","Gujarat"),
+   ("HR","Haryana"),
+   ("HP","Himachal Pradesh"),
+   ("JK","Jammu and Kashmir"),
+   ("JH","Jharkhand"),
+   ("KA","Karnataka"),
+   ("KL","Kerala"),
+   ("LA","Ladakh"),
+   ("LD","Lakshadweep"),
+   ("MP","Madhya Pradesh"),
+   ("MH","Maharashtra"),
+   ("MN","Manipur"),
+   ("ML","Meghalaya"),
+   ("MZ","Mizoram"),
+   ("NL","Nagaland"),
+   ("OD","Odisha"),
+   ("PB","Punjab"),
+   ("PY","Pondicherry"),
+   ("RJ","Rajasthan"),
+   ("SK","Sikkim"),
+   ("TN","Tamil Nadu"),
+   ("TS","Telangana"),
+   ("TR","Tripura"),
+   ("UP","Uttar Pradesh"),
+   ("UK","Uttarakhand"),
+   ("WB","West Bengal")
+)
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+    def __str__(self):
+        return str(self.id)
+
+CATEGORY_CHOICES ={
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW','Bottom Wear')
+}
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='prodcting',null = True)
+    
+    def __str__(self):
+        return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+STATUS_CHOICES = {
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('Arriving','Arriving'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel')
+
+}
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    
